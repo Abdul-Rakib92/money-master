76db1262a4b18828e8459d20b1bf5083a099382d
Abdul-Rakib92 <rakibctgu@gmail.com>
2022-02-17 16:22:36 +0600

    saving amount added

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 // handle calculate button event
 
+
 document.getElementById('calculate-button').addEventListener('click', function(){
     const incomeInput = document.getElementById('income-input');
     const incomeAmountText = incomeInput.value;
@@ -15,5 +16,31 @@ document.getElementById('calculate-button').addEventListener('click', function()
     totalExpenses.innerText = parseFloat(foodExpense.value) + parseFloat(rentExpense.value) + parseFloat(clothExpense.value);;
 
     // update balance
-    document.getElementById('balance-total').innerText = incomeAmount - totalExpenses.innerText;
+    const balanceTotal = document.getElementById('balance-total').innerText = incomeAmount - totalExpenses.innerText;
 });
+
+document.getElementById('save-button').addEventListener('click', function(){
+    const saveInput = document.getElementById('save-input');
+    const saveTotalText = saveInput.value;
+    const saveTotal = parseFloat(saveTotalText);
+
+    const balanceTotal = document.getElementById('balance-total').innerText
+
+    const save = balanceTotal * 0.2;
+
+
+    // update saving amount
+    const savingAmount = document.getElementById('saving-amount');
+    const savingAmountText = savingAmount.innerText;
+    savingAmount.innerText = save
+    // const save = parseFloat(savingAmountText);
+    // savingAmount.innerText = parseFloat()
+
+    
+    // const save = balanceTotal * 0.2;
+
+    // savingAmount.innerText = save - saveTotal;
+
+    // console.log('fas',  );
+
+})
