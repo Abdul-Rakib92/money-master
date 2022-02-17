1931ea00f71d9de0bb33b81ea4bd835bbbe36012
Abdul-Rakib92 <rakibctgu@gmail.com>
2022-02-17 17:34:00 +0600

    remaining balance added

--- js/app.js
+++ js/app.js
@@ -18,21 +18,38 @@ document.getElementById('calculate-button').addEventListener('click', function()
     // update balance
     const balanceTotal = document.getElementById('balance-total').innerText = incomeAmount - totalExpenses.innerText;
 });
+// function percent(num1, num2) {
+//     const result = num1 /100 * num2;
+//     return result;
+// }
 
 document.getElementById('save-button').addEventListener('click', function(){
     const saveInput = document.getElementById('save-input');
     const saveTotalText = saveInput.value;
     const saveTotal = parseFloat(saveTotalText);
 
-    const balanceTotal = document.getElementById('balance-total').innerText
+    const balanceTotal = document.getElementById('balance-total');
+    const balanceAmountText = balanceTotal.innerText;
+    const balance = parseFloat(balanceAmountText);
 
-    const save = balanceTotal * 0.2;
+
+
+    // const save = percent(balance * saveTotal);
 
 
     // update saving amount
-    const savingAmount = document.getElementById('saving-amount');
+    /* const savingAmount = document.getElementById('saving-amount');
     const savingAmountText = savingAmount.innerText;
-    savingAmount.innerText = save
+    const saving = parseFloat(savingAmountText); */
+    const totalSaving = balance / 100 * saveTotal;
+    const remainingBalance = balance - totalSaving;
+
+
+    // update remaining balance
+    const savingAmount = document.getElementById('saving-amount').innerText = totalSaving;
+    document.getElementById('remaining-balance').innerText = remainingBalance;
+
+
     // const save = parseFloat(savingAmountText);
     // savingAmount.innerText = parseFloat()
 
